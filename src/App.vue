0483a47e873639f737ea77e6ce8368f579af1500
AntonioMadridC <antoniomadridc@gmail.com>
2023-05-31 21:03:50 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav
+      class="navbar bg-dark border-bottom border-bottom-dark mb-5 justify-content-center"
+      data-bs-theme="dark"
+    >
+      <h1 class="text-center"><a class="link-light link-underline link-underline-opacity-0" href="#">Tienda Bit-Game</a></h1>
+    </nav>
+
+    <div class="alert alert-info mb-5" role="alert">
+      <h1>¡Bienvenido!</h1>
+      <hr>
+      <p>A continuación encontrarás la lista de tus juegos favoritos</p>
+    </div>
+
+    <div class="container">
+      <FavoriteGames :games="games" @remove="removeGame"></FavoriteGames>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FavoriteGames from "./components/favoriteGames.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FavoriteGames,
+  },
+  data: function () {
+    return {
+      games: [
+        "Arena of valor",
+        "Pokemon Unite",
+        "Donkey Kong",
+        "Heroes of the storm",
+        "Valorant",
+      ],
+    };
+  },
+  methods: {
+    removeGame(index) {
+      this.games.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,6 +53,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
 }
 </style>
---
+++ src/components/favoriteGames.vue
@@ -0,0 +1,52 @@
+<template>
+  <table class="table table-dark table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Juego</th>
+        <th scope="col">Eliminar juego de tu lista</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(game, index) in games" :key="game">
+        <th scope="row">{{ index + 1 }}</th>
+        <td>{{ game }}</td>
+        <td>
+          <button class="btn btn-danger" @click="remove(index)">
+            Eliminar
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "favorite-games",
+  props: {
+    games: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: function () {
+    return {};
+  },
+  // computed: {},
+  methods: {
+    remove(index) {
+      this.$emit("remove", index);
+    },
+  },
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
+</script>
+
+<style scoped>
+</style>
